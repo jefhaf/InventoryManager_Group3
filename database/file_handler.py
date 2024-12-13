146d7e7cf5f2d51c9c97e4f27ad1958c1cfd4281
Tequali <dschuhknecht17@gmail.com>
2024-12-13 14:59:55 +0100

    corrected the information input to save

--- database/file_handler.py
+++ database/file_handler.py
@@ -5,9 +5,8 @@ import json
 class FileHandler:
     @staticmethod
     def save_to_json(products):
-        products_dicts = [product.to_dict() for product in products]
         with open("products.json", "w") as file:
-            json.dump(products_dicts, file, indent=4)
+            json.dump(products, file, indent=4)
 
     @staticmethod
     def load_from_json():
@@ -20,9 +19,8 @@ class FileHandler:
 
     @staticmethod
     def save_users_to_json(users):
-        users_dicts = [user.to_dict() for user in users]
         with open("users.json", "w") as file:
-            json.dump(users_dicts, file, indent=4)
+            json.dump(users, file, indent=4)
 
     @staticmethod
     def load_users_from_json():
