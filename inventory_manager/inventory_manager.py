14bb7ae7bae715387662d43504dc3d5f934add83
Jay <Jay@jaylord.com>
2024-12-13 16:02:09 +0100

    Shiny stuff yogit add .!

--- inventory_manager/inventory_manager.py
+++ inventory_manager/inventory_manager.py
@@ -26,20 +26,29 @@ class InventoryManager:
         print("Please give details about new product:")
         name = input("name: ")
         model = input("Model: ")
-        category = input("Category (electronics, food, apparel, household, toys, books): ")
+        category = input(
+            "Category (electronics, food, apparel, household, toys, books): "
+        )
         quantity = int(input("Quantity: "))
         price = float(input("Price: "))
         colour = input("Colour: ")
 
-        kwargs = {"name": name, "model": model, "category": category, "quantity": quantity, "price": price, "colour": colour}
+        kwargs = {
+            "name": name,
+            "model": model,
+            "category": category,
+            "quantity": quantity,
+            "price": price,
+            "colour": colour,
+        }
 
-        new_product = product_factory(kwargs)
+        new_product = product_factory(**kwargs)
 
         new_product_info = new_product.to_dict()
 
         for item in self.product_database:
-            if item["table_name"] == "electronics":
-                item["records"].append(new_product_info) 
+            if item["table_name"] == category:
+                item["records"].append(new_product_info)
 
         """ if category.lower() == "electronics":
             new_product = Electronics(name=name, model=model, warranty_years=warranty_years, quantity=quantity, price=price, colour=colour)
@@ -64,52 +73,59 @@ class InventoryManager:
 
 
 def product_factory(**kwargs):
-
     """Factory method to create product objects based on category"""
 
-    if kwargs["category"].lower() == "electronic":
+    if kwargs["category"].lower() == "electronics":
+
 
         warranty_years = input("Warranty in years: ")
 
         kwargs["warranty_years"] = warranty_years
 
+        del kwargs["category"]
+
         return Electronics(**kwargs)
-    
+
     elif kwargs["category"].lower() == "food":
-        
+
         expiration_date = input("Expiration date")
 
         kwargs["expiration_date"] = expiration_date
+        del kwargs["category"]
 
         return Food(**kwargs)
-    
+
     elif kwargs["category"].lower() == "apparel":
 
+        del kwargs["category"]
+
         return Apparel(**kwargs)
-    
+
     elif kwargs["category"].lower() == "household":
 
+        del kwargs["category"]
+
         return Household(**kwargs)
-    
+
     elif kwargs["category"].lower() == "toys":
 
+        del kwargs["category"]
+
         return Toys(**kwargs)
-    
+
     elif kwargs["category"].lower() == "books":
+
+        del kwargs["category"]
 
         author = input("Author: ")
 
         kwargs["author"] = author
 
         return Book(**kwargs)
-    
+
     else:
 
         raise ValueError("Invalid category")
-
-
-
-
 
 
 def im_test():
@@ -122,7 +138,6 @@ def im_test():
     print()
 
     print(im.product_database)
-
 
 
 im_test()
