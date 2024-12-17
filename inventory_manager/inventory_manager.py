b26438743e645509815cfababe3b65508c8c7261
Jay <Jay@jaylord.com>
2024-12-17 14:52:51 +0100

    Add remove_product, updated_quantity, get_total_inventory_value, refactor product_factory

--- inventory_manager/inventory_manager.py
+++ inventory_manager/inventory_manager.py
@@ -17,58 +17,152 @@ class InventoryManager:
         # Use method to load the database and store in attribute self.product_database
         self.product_database = FileHandler.load_from_json()
 
-    def search_product(self):
+    def search_product_by_user(self):
+        initial_search = True
+
+        kwargs = {}
+        while True:
+            #  user input
+            print("Please provide one or more search parameters")
+            make_user_input = input("specify make or leave empty: ")
+            if make_user_input:
+                kwargs["make"] = make_user_input
+
+            model_user_input = input("specify model or leave empty: ")
+            if model_user_input:
+                kwargs["model"] = model_user_input
+
+            low_price_user_input = input(
+                "specify lower price limit or leave empty: "
+            )
+            if low_price_user_input:
+                kwargs["model"] = low_price_user_input
+
+            high_price_user_input = input(
+                "specify upper price limitor leave empty: "
+            )
+            if high_price_user_input:
+                kwargs["model"] = high_price_user_input
+
+            category_user_input = input("specify category or leave empty: ")
+            if category_user_input:
+                kwargs["model"] = category_user_input
+
+            colour_user_input = input("specify colour or leave empty: ")
+            if colour_user_input:
+                kwargs["model"] = colour_user_input
+
+            self.search_product(**kwargs)
+
+        # call search function
+
+    def search_product(self, **kwargs):
         """Search for product in inventory"""
-        pass
+        search_results = []
+
+    def is_exist_product(self, make: str, model: str) -> bool:
+
+        for item in self.product_database:
+
+            if any(
+                product["make"] == make and product["model"] == model
+                for product in item["records"]
+            ):
+
+                return True
+
+        return False
 
     def add_product(self):
         """add new product to inventory"""
         print("Please give details about new product:")
         make = input("make: ")
         model = input("Model: ")
-        category = input(
-            "Category (electronics, food, apparel, household, toys, books): "
-        )
-        quantity = int(input("Quantity: "))
-        price = float(input("Price: "))
-        colour = input("Colour: ")
 
-        kwargs = {
-            "make": make,
-            "model": model,
-            "category": category,
-            "quantity": quantity,
-            "price": price,
-            "colour": colour,
-        }
+        if not self.is_exist_product(make, model):
+            category = input(
+                "Category (electronics, food, apparel, household, toys, books): "
+            )
+            quantity = int(input("Quantity: "))
+            price = float(input("Price: "))
+            colour = input("Colour: ")
 
-        new_product = product_factory(**kwargs)
+            kwargs = {
+                "make": make,
+                "model": model,
+                "category": category,
+                "quantity": quantity,
+                "price": price,
+                "colour": colour,
+            }
 
-        new_product_info = new_product.to_dict()
+            new_product = product_factory(**kwargs)
 
-        for item in self.product_database:
-            if item["table_name"] == category:
-                item["records"].append(new_product_info)
-
-        """ if category.lower() == "electronics":
-            new_product = Electronics(name=name, model=model, warranty_years=warranty_years, quantity=quantity, price=price, colour=colour)
             new_product_info = new_product.to_dict()
-            for item in self.product_database:
-                if item["table_name"] == "electronics":
-                    item["records"].append(new_product_info) """
 
-        FileHandler.save_to_json(self.product_database)
+            for item in self.product_database:
+                if item["table_name"] == category:
+                    item["records"].append(new_product_info)
+
+            """ if category.lower() == "electronics":
+                new_product = Electronics(name=name, model=model, warranty_years=warranty_years, quantity=quantity, price=price, colour=colour)
+                new_product_info = new_product.to_dict()
+                for item in self.product_database:
+                    if item["table_name"] == "electronics":
+                        item["records"].append(new_product_info) """
+
+            FileHandler.save_to_json(self.product_database)
 
     def remove_product(self):
         """Remove existing product from inventory"""
-        pass
+        print("Please give details about product to delete:")
+        make = input("make: ")
+        model = input("Model: ")
+
+        if self.is_exist_product(make, model):
+
+            for item in self.product_database:
+
+                for product in item["records"]:
+
+                    if product["make"] == make and product["model"] == model:
+                        item["records"].remove(product)
+                        FileHandler.save_to_json(self.product_database)
 
     def update_quantity(self):
         """Update quantity of a product in inventory"""
-        pass
+        make = input("make: ")
+        model = input("Model: ")
+
+        if self.is_exist_product(make, model):
+
+            for item in self.product_database:
+
+                for product in item["records"]:
+
+                    if product["make"] == make and product["model"] == model:
+                        new_quantity = input(
+                            f"Current quantity: {product['quantity']}\nNew quantity: "
+                        )
+                        new_quantity = int(new_quantity)
+                        product["quantity"] = new_quantity
+                        FileHandler.save_to_json(self.product_database)
 
     def get_total_inventory_value(self):
         """Calculate total value of the entire inventory"""
+        total_inventory_value = 0
+
+        for category_dabase in self.product_database:
+            for record in category_dabase["records"]:
+                del record["id"]
+
+                price_calc_object = product_factory(**record)
+                total_inventory_value += price_calc_object.get_total_price()
+
+        return total_inventory_value
+
+    def validate_database(self):
+        """Check for any missing data or wrong datatypes in all entries of database"""
         pass
 
 
@@ -83,17 +177,19 @@ def product_factory(**kwargs):
     try:
         if category.lower() == "electronics":
 
-            warranty_years = input("Warranty in years: ")
+            if not kwargs["warranty_years"]:
+                warranty_years = input("Warranty in years: ")
 
-            kwargs["warranty_years"] = warranty_years
+                kwargs["warranty_years"] = warranty_years
 
             return Electronics(**kwargs)
 
         elif category.lower() == "food":
 
-            expiration_date = input("Expiration date")
+            if not kwargs["Expiration date"]:
+                expiration_date = input("Expiration date")
 
-            kwargs["expiration_date"] = expiration_date
+                kwargs["expiration_date"] = expiration_date
 
             return Food(**kwargs)
 
@@ -111,9 +207,11 @@ def product_factory(**kwargs):
 
         elif category.lower() == "book":
 
-            author = input("Author: ")
+            if not kwargs["Author"]:
 
-            kwargs["author"] = author
+                author = input("Author: ")
+
+                kwargs["author"] = author
 
             return Book(**kwargs)
 
@@ -128,31 +226,15 @@ def product_factory(**kwargs):
         return None
 
 
-def is_exist_product(self, make: str, model: str) -> bool:
-
-    for item in self.product_database:
-
-        if any(
-            product["make"] == make
-            and product["model"] == model
-            for product in item["records"]
-        ):
-
-            return True
-
-    return False
-
-
 def im_test():
     # Create insctance of InventoryManager
     im = InventoryManager()
 
     print(im.product_database)
 
-    im.add_product()
+    # im.add_product()
+    print(im.get_total_inventory_value())
     print()
-
-    print(im.product_database)
 
 
 im_test()
